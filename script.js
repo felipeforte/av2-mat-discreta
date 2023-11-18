fa8a47d1776dbed878ec74f25f31492b13f6f7af
Felipe Forte <felipeforte@protonmail.com>
2023-11-18 11:32:18 -0300

    mais coisas

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function retornaValor(hi, hf) {
         if (diff[0]<3) {
             valor += 5;
         } else {
-            valor += 5 + ((diff[0]-3) + (diff[1]>0)) * 2.5
+            valor += 5 + ((diff[0]-3) + (diff[1]>0)) * 2.5;
         }
     } else {
         if (diff[1]>15) {
@@ -26,4 +26,49 @@ function retornaValor(hi, hf) {
         }
     }
     return valor;
+}
+
+function checarEstado(placa) {
+    let placa_regex = /^[A-Z]{3}\d[A-Z]\d{2}$/;
+    let placa_regex_antiga = /^[A-Z]{3}[-]\d{4}$/;
+
+    if (placa_regex_antiga.test(placa)) {
+        let aux = "";
+        for (let i=0;i<placa.length;i++) {
+            if (i == 5) {
+                let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+                aux += letras[placa.charAt(i)];
+            } else if (placa.charAt(i) != "-") {
+                aux += placa.charAt(i);
+            }
+        }
+        console.log("Placa antiga, convertendo de \"" + placa + "\" para \"" + aux + "\"")
+        placa = aux;
+    } else if (!placa_regex.test(placa)) {
+        console.log("Padrão de placa inválido, abortando...");
+        return;
+    }
+
+    const intervalos = [
+        // Pará, 0-10
+        ["JTA","JWE"],
+        ["NSE","NTC"],
+        ["JTA","JWE"],
+        ["OBT","OCA"],
+        ["OFI","OFW"],
+        ["OSW","OTZ"],
+        ["OFI","OFW"],
+        ["OSW","OTZ"],
+        ["QDA","QEZ"],
+        ["QVA","QVZ"],
+        ["RWK","RXD"],
+        // Roraima, 11-13
+        ["NAH","NBA"],
+        ["NUH","NUL"],
+        ["RZA","RZD"],
+        // Amapá, 14-16
+        ["NEI","NFB"]
+        ["QLN","QLT"]
+        ["SAK","SAM"]
+    ]
 }
